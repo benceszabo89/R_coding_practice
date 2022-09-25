1343536b52dd692f54aab1912ad0712b84c6890d
benceszabo89 <bence.szabo89@gmail.com>
2022-09-25 20:01:14 +0200

    Add files via upload

---
+++ Sample_assignment.R
@@ -0,0 +1,26 @@
+
+# In this short test assignment we will demo how to create a local copy of an assignment and send a pull request.
+
+# Task 1: Set the working directory to the 'R_coding_practice' library
+
+
+# Task 2: Import the ggplot2 library
+
+
+# Task 3: Use the mtcars dataset (it is a built-in dataset to R) and create a histogram of the 'Miles/(US) gallon' using ggplot().
+# For more information about the dataset type ?mtcars.
+
+
+# Task 4: Create a commit and push your work.
+
+
+# Task 5: Read in the iris.csv dataset
+
+
+# Task 6: Create a barplot of the number of observations by species.
+
+
+# Task 7: Create a commit and push your work.
+
+
+# Task 8: Create a pull request.
