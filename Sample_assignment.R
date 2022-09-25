fda7936b0f030c86d07b0ea532258aab35b780b2
viktoriakonya <viktoria.konya@gmail.com>
2022-09-25 20:11:38 +0200

    Tasks 1-4 completed.

--- Sample_assignment.R
+++ Sample_assignment.R
@@ -1,15 +1,17 @@
 
-# In this short test assignment we will demo how to create a local copy of an assignment and send a pull request.
+# In this short test assignment we will demo how to create a local copy of an assinment and send a pull request.
 
 # Task 1: Set the working directory to the 'R_coding_practice' library
-
+setwd('C:/Users/User/Documents/GitHub/R_coding_practice')
 
 # Task 2: Import the ggplot2 library
-
+library(ggplot2)
 
 # Task 3: Use the mtcars dataset (it is a built-in dataset to R) and create a histogram of the 'Miles/(US) gallon' using ggplot().
 # For more information about the dataset type ?mtcars.
-
+ggplot(data = mtcars, aes(x=mpg)) +
+  geom_histogram(binwidth=5, fill = "blue") +
+  theme_bw()
 
 # Task 4: Create a commit and push your work.
 
